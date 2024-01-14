5b19c4ca34a3b6c5f6c211e8d2b93e4665a7b02c
keramsky <keramsky@yahoo.com>
2024-01-14 22:12:13 +0100

    add a start button

--- js/javascript.js
+++ js/javascript.js
@@ -1,22 +1,23 @@
-const colorThemeElement = document.querySelector('.js-color-theme');
-const pageElement = document.querySelector('.js-container');
-const boxElement = document.querySelector('.js-box');
+const colorThemeButtonElement = document.querySelector('.js-color-theme-button');
+const overlayElement = document.querySelector('.js-overlay');
+const boardContainerElement = document.querySelector('.js-board-container')
+const boardElement = document.querySelector('.js-board');
 
 let currentPageTheme = "white";
 
 function changeToBlackTheme(){
-    pageElement.style.backgroundColor = "#736c6c";
-    boxElement.style.backgroundColor = "#8c8a89";
-    boxElement.style.boxShadow = "0px 5px 30px rgba(255, 255, 255, 3)";
+    overlayElement.style.backgroundColor = "#736c6c";
+    boardElement.style.backgroundColor = "#8c8a89";
+    boardContainerElement.style.boxShadow = "0px 5px 30px rgba(255, 255, 255, 3)";
 }
 
 function changeToWhiteTheme(){
-    pageElement.style.backgroundColor = "white";
-    boxElement.style.backgroundColor = "white";
-    boxElement.style.boxShadow = "0px 5px 30px rgba(0, 0, 0, 3)";
+    overlayElement.style.backgroundColor = "white";
+    boardElement.style.backgroundColor = "white";
+    boardContainerElement.style.boxShadow = "0px 5px 30px rgba(0, 0, 0, 3)";
 }
 
-colorThemeElement.addEventListener('click', function(){
+colorThemeButtonElement.addEventListener('click', function(){
     if(currentPageTheme === "white"){
         changeToBlackTheme();
         currentPageTheme = "black";
